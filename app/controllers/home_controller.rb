afb4ab20e8b97e0e889aee7aa9b281adf0a58514
v_berkela <v_berkela@ukr.net>
2021-07-20 15:39:07 +0300

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+    @information = "List of countries with their capitals from https://restcountries.eu/rest/v2"
+    require "http"
+    @response = HTTP.get("https://restcountries.eu/rest/v2/all")
+    @parsed_response = JSON.parse(@response)
+
+  end
+end
